29b0d69cba79808221f5c6a3fce0a2702d5ec75c
Audra Rodríguez <audrarg@hotmail.com>
2019-08-30 10:09:36 -0600

    Seleccionar favoritos

--- conversorDivisas/conversorDivisas.js
+++ conversorDivisas/conversorDivisas.js
@@ -1,11 +1,21 @@
-function cambioIMG() {
+var listaFavoritos = [];
 
-    if (document.getElementById("imgClickAndChange").src == "conversorDivisasimg/star.png") 
-    {
-        document.getElementById("imgClickAndChange").src = "conversorDivisasimg/starY.png";
-    }
-    else 
-    {
-        document.getElementById("imgClickAndChange").src = "conversorDivisasimg/star.png";
-    }
+function cambioIMG(input){
+	var x  = input.value;
+	if(x == "noSelect"){
+		input.src = "conversorDivisasimg/starY.png";
+		input.value = "select";
+		listaFavoritos.push(input.name);
+	}else{
+		input.src = "conversorDivisasimg/star.png";
+		input.value = "noSelect";
+		
+		for( var i = 0; i < listaFavoritos.length; i++){ 
+		   if ( listaFavoritos[i] === input.name) {
+		   		document.getElementsByName("text"+listaFavoritos[i])[0].value = "";
+		     	listaFavoritos.splice(i, 1); 
+		     	i--;
+		   }
+		}
+	}
 }
